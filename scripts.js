02b7f4c1ec3ff4245984c08b934f84d91bee7c81
WesleyTMarques <wesleytmarques@outlook.com>
2024-01-15 13:10:53 -0300

    Ajustes develop

--- scripts.js
+++ scripts.js
@@ -1,17 +1,14 @@
 function generateNumber() {
     const min = Math.ceil(document.querySelector('.first-number').value)
     const max = Math.floor(document.querySelector('.second-number').value)
-    const palpite = document.querySelector('.shotNumber').value
+    // const palpite = document.querySelector('.shotNumber').value
+    let answer = document.querySelector('.result')
 
     const result = Math.floor(Math.random() * (max - min + 1) + min)
 
-    if (palpite == 0) {
-        alert("O número sorteado foi: " + result)
-    } else {
-        if (palpite == result) {
-            alert("Parabens, você acertou o número sorteado!\nNúmero sorte: " + result + "\nSeu palpite: " + palpite)
-        } else {
-            alert("Puxa, seu palpite não está correto, mais sorte na próxima jogada!\nNúmero sorteado: " + result + "\nSeu palpite: " + palpite)
-        }
+    if(min === 0 && max == 0){
+        alert("Digite um valor minimo e um maximo para realizar o sorteio")
+    } else{
+        answer.innerHTML = `O numero sorteado é ${result}`
     }
 }
